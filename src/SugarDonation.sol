44074fa19e29356abc0627e3dc48a9328a1c0a74
Dante4rt <rxmxdhxni@gmail.com>
2025-03-24 15:10:17 +0700

    fix: update Solidity pragma and refactor fee handling in SugarDonation contract

--- src/SugarDonation.sol
+++ src/SugarDonation.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,21 +8,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract SugarDonation is Ownable {
     event DonationReceived(address indexed donor, address indexed creator, address token, uint256 amount);
     event Withdraw(address indexed recipient, address token, uint256 amount);
-    event EtherWithdrawn(address indexed recipient, uint256 amount);
     event TokenWhitelisted(address indexed creator, address token, bool status);
 
     mapping(address => mapping(address => bool)) public whitelistedTokens;
-    mapping(address => mapping(address =>uint256)) public creatorBalances;
-    mapping(address => mapping(address =>uint256)) public ownerFee;
-    
-
+    mapping(address => mapping(address => uint256)) public creatorBalances;
+    mapping(address => mapping(address => uint256)) public ownerFees;
 
     bool private _notEntered;
 
     uint256 public constant FEE_PERCENTAGE = 2; // fee 2%
 
     constructor() Ownable(msg.sender) {
-        
         _notEntered = true;
     }
 
@@ -38,36 +34,36 @@ contract SugarDonation is Ownable {
         emit TokenWhitelisted(msg.sender, token, status);
     }
 
-
-    function donate(address creator, address token, uint256 amount) external  nonReentrant payable{
+    function donate(address creator, address token, uint256 amount) external payable nonReentrant {
         require(amount > 0, "Amount must be greater than 0");
         require(whitelistedTokens[creator][token], "Token not whitelisted by the creator");
 
         uint256 fee = (amount * FEE_PERCENTAGE) / 100;
         uint256 amountAfterFee = amount - fee;
-        if(token == address(0)){
+
+        ownerFees[owner()][token] += fee;
+        creatorBalances[creator][token] += amountAfterFee;
+
+        if (token == address(0)) {
             require(msg.value == amount, "Ether value must be equal to amount");
         } else {
             require(msg.value == 0, "Do not send ETH with ERC20 donation");
             bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
             require(success, "ERC20 transfer failed");
         }
-        ownerFee[owner()][token] += fee;
-        address currentOwner = owner();
-        creatorBalances[creator][token] += amountAfterFee;
-        
+
         emit DonationReceived(msg.sender, creator, token, amountAfterFee);
     }
 
-function withdrawOwnerFees(address token) external nonReentrant onlyOwner {
+    function withdrawOwnerFees(address token) external nonReentrant onlyOwner {
         address currentOwner = owner();
-        uint256 amount = ownerFee[currentOwner][token];
+        uint256 amount = ownerFees[currentOwner][token];
         require(amount > 0, "No fees to withdraw");
 
-        ownerFee[currentOwner][token] = 0;
+        ownerFees[currentOwner][token] = 0;
 
         if (token == address(0)) {
-            (bool success, ) = currentOwner.call{value: amount}("");
+            (bool success,) = currentOwner.call{value: amount}("");
             require(success, "ETH transfer failed");
         } else {
             bool success = IERC20(token).transfer(currentOwner, amount);
@@ -84,7 +80,7 @@ function withdrawOwnerFees(address token) external nonReentrant onlyOwner {
         creatorBalances[msg.sender][token] = 0;
 
         if (token == address(0)) {
-            (bool success, ) = msg.sender.call{value: amount}("");
+            (bool success,) = msg.sender.call{value: amount}("");
             require(success, "ETH transfer failed");
         } else {
             bool success = IERC20(token).transfer(msg.sender, amount);
@@ -94,9 +90,7 @@ function withdrawOwnerFees(address token) external nonReentrant onlyOwner {
         emit Withdraw(msg.sender, token, amount);
     }
 
-     /** @notice Check if a token is whitelisted by a creator */
     function isTokenWhitelisted(address creator, address token) external view returns (bool) {
         return whitelistedTokens[creator][token];
     }
 }
-
--- test/SugarDonation.t.sol
+++ test/SugarDonation.t.sol
@@ -2,15 +2,15 @@
 
 pragma solidity 0.8.20;
 
-import "lib/forge-std/src/Test.sol";
+import "forge-std/Test.sol";
 import "../src/SugarDonation.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 1000000 * 10 ** 18);
-
     }
+
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
@@ -33,37 +33,57 @@ contract SugarDonationTest is Test {
         token.transfer(donor, 1000 * 10 ** 18);
     }
 
-    // function testWhitelistToken() public {
-    //     vm.prank(creator);
-    //     sugarDonation.whitelistToken(address(token));
+    function test_SetWhitelistToken() public {
+        vm.prank(creator);
+        sugarDonation.setWhitelistToken(address(token), true);
 
-    //     assertTrue(sugarDonation.isTokenWhitelisted(creator, address(token)), "Token should be whitelisted");
-    // }
+        assertTrue(sugarDonation.isTokenWhitelisted(creator, address(token)), "Token should be whitelisted");
 
-    function testDonate() public {
+        vm.prank(creator);
+        sugarDonation.setWhitelistToken(address(token), false);
+        assertFalse(sugarDonation.isTokenWhitelisted(creator, address(token)), "Token should not be whitelisted");
+    }
+
+    function test_DonateERC20() public {
         uint256 donationAmount = 100 * 10 ** 18;
         uint256 fee = (donationAmount * sugarDonation.FEE_PERCENTAGE()) / 100;
         uint256 amountAfterFee = donationAmount - fee;
-        
-        console.log(donor.balance,"<--creator");
+
         vm.prank(creator);
-        sugarDonation.whitelistToken(address(token));
+        sugarDonation.setWhitelistToken(address(token), true);
 
         vm.prank(donor);
         token.approve(address(sugarDonation), donationAmount);
 
-
         vm.prank(donor);
-        // vm.deal(donor,1 ether);
         sugarDonation.donate(creator, address(token), donationAmount);
 
-        assertEq(token.balanceOf(address(deployer)), fee, "Owner fee should be 2 MTK");
-        assertEq(sugarDonation.creatorBalances(creator, address(token)), amountAfterFee, "Creator balance should be 98 MTK");
-        // assertEq(token.balanceOf(deployer), 2 * 10 ** 18, "Deployer should receive 2 MTK as fee");
-        // assertEq(token.balanceOf(creator), 98 * 10 ** 18, "Creator should receive 98 MTK");
+        assertEq(sugarDonation.ownerFees(deployer, address(token)), fee, "Owner fee should be correct");
+        assertEq(
+            sugarDonation.creatorBalances(creator, address(token)), amountAfterFee, "Creator balance should be correct"
+        );
     }
 
-    function testDonateWithNonWhitelistedToken() public {
+    function test_DonateETH() public {
+        uint256 donationAmount = 1 ether;
+        uint256 fee = (donationAmount * sugarDonation.FEE_PERCENTAGE()) / 100;
+        uint256 amountAfterFee = donationAmount - fee;
+
+        vm.prank(creator);
+        sugarDonation.setWhitelistToken(address(0), true);
+
+        vm.deal(donor, donationAmount);
+
+        vm.prank(donor);
+        sugarDonation.donate{value: donationAmount}(creator, address(0), donationAmount);
+
+        assertEq(sugarDonation.ownerFees(deployer, address(0)), fee, "Owner fee should be correct");
+        assertEq(
+            sugarDonation.creatorBalances(creator, address(0)), amountAfterFee, "Creator balance should be correct"
+        );
+    }
+
+    function test_DonateWithNonWhitelistedToken() public {
         vm.prank(donor);
         token.approve(address(sugarDonation), 100 * 10 ** 18);
 
@@ -72,62 +92,107 @@ contract SugarDonationTest is Test {
         sugarDonation.donate(creator, address(token), 100 * 10 ** 18);
     }
 
-     function testWithdrawOwnerFees() public {
-        vm.prank(creator);
-        sugarDonation.whitelistToken(address(token));
-
+    function test_WithdrawOwnerFeesERC20() public {
         uint256 donationAmount = 100 * 10 ** 18;
         uint256 fee = (donationAmount * sugarDonation.FEE_PERCENTAGE()) / 100;
 
-        token.mint(address(sugarDonation), fee);
-
-        vm.deal(donor, 1 ether);
+        vm.prank(creator);
+        sugarDonation.setWhitelistToken(address(token), true);
 
         vm.prank(donor);
         token.approve(address(sugarDonation), donationAmount);
         vm.prank(donor);
-        sugarDonation.donate{value: 1 ether}(
-            creator,
-            address(token),
-            donationAmount
-        );
+        sugarDonation.donate(creator, address(token), donationAmount);
 
-        assertEq(sugarDonation.ownerFee(deployer, address(token)), fee);
+        assertEq(sugarDonation.ownerFees(deployer, address(token)), fee, "Owner fee should be set correctly");
 
         uint256 ownerBalanceBefore = token.balanceOf(deployer);
 
         vm.prank(deployer);
         sugarDonation.withdrawOwnerFees(address(token));
 
-        assertEq(
-            sugarDonation.ownerFee(deployer, address(token)),
-            0,
-            "Owner fee should be zero after withdrawal"
-        );
-        assertEq(
-            token.balanceOf(deployer),
-            ownerBalanceBefore + fee,
-            "Owner should receive tokens"
-        );
+        assertEq(sugarDonation.ownerFees(deployer, address(token)), 0, "Owner fee should be zero after withdrawal");
+        assertEq(token.balanceOf(deployer), ownerBalanceBefore + fee, "Owner should receive tokens");
     }
 
-    function testWithdrawOwnerFeesWithNoFees() public {
+    function test_WithdrawOwnerFeesETH() public {
+        uint256 donationAmount = 1 ether;
+        uint256 fee = (donationAmount * sugarDonation.FEE_PERCENTAGE()) / 100;
+
+        vm.prank(creator);
+        sugarDonation.setWhitelistToken(address(0), true);
+
+        vm.deal(donor, donationAmount);
+        vm.prank(donor);
+        sugarDonation.donate{value: donationAmount}(creator, address(0), donationAmount);
+
+        assertEq(sugarDonation.ownerFees(deployer, address(0)), fee, "Owner fee should be set correctly");
+
+        uint256 ownerBalanceBefore = deployer.balance;
+
+        vm.prank(deployer);
+        sugarDonation.withdrawOwnerFees(address(0));
+
+        assertEq(sugarDonation.ownerFees(deployer, address(0)), 0, "Owner fee should be zero after withdrawal");
+        assertEq(deployer.balance, ownerBalanceBefore + fee, "Owner should receive ETH");
+    }
+
+    function test_WithdrawOwnerFeesWithNoFees() public {
         vm.prank(deployer);
         vm.expectRevert("No fees to withdraw");
         sugarDonation.withdrawOwnerFees(address(token));
     }
 
-    function testWithdrawEther() public {
-        uint256 amount = 1 ether;
-        vm.deal(donor, amount);
+    function test_WithdrawCreatorFundsERC20() public {
+        uint256 donationAmount = 100 * 10 ** 18;
+        uint256 fee = (donationAmount * sugarDonation.FEE_PERCENTAGE()) / 100;
+        uint256 amountAfterFee = donationAmount - fee;
 
-        uint256 ownerBalanceBefore = deployer.balance;
+        vm.prank(creator);
+        sugarDonation.setWhitelistToken(address(token), true);
 
-        vm.prank(deployer);
-        sugarDonation.withdrawEther(deployer);
+        vm.prank(donor);
+        token.approve(address(sugarDonation), donationAmount);
+        vm.prank(donor);
+        sugarDonation.donate(creator, address(token), donationAmount);
 
-        assertEq(deployer.balance, ownerBalanceBefore + amount);
+        uint256 creatorBalanceBefore = token.balanceOf(creator);
+
+        vm.prank(creator);
+        sugarDonation.withdrawCreatorFunds(address(token));
+
+        assertEq(
+            sugarDonation.creatorBalances(creator, address(token)), 0, "Creator balance should be zero after withdrawal"
+        );
+        assertEq(token.balanceOf(creator), creatorBalanceBefore + amountAfterFee, "Creator should receive tokens");
     }
-    
-}
 
+    function test_WithdrawCreatorFundsETH() public {
+        uint256 donationAmount = 1 ether;
+        uint256 fee = (donationAmount * sugarDonation.FEE_PERCENTAGE()) / 100;
+        uint256 amountAfterFee = donationAmount - fee;
+
+        vm.prank(creator);
+        sugarDonation.setWhitelistToken(address(0), true);
+
+        vm.deal(donor, donationAmount);
+        vm.prank(donor);
+        sugarDonation.donate{value: donationAmount}(creator, address(0), donationAmount);
+
+        uint256 creatorBalanceBefore = creator.balance;
+
+        vm.prank(creator);
+        sugarDonation.withdrawCreatorFunds(address(0));
+
+        assertEq(
+            sugarDonation.creatorBalances(creator, address(0)), 0, "Creator balance should be zero after withdrawal"
+        );
+        assertEq(creator.balance, creatorBalanceBefore + amountAfterFee, "Creator should receive ETH");
+    }
+
+    function test_WithdrawCreatorFundsWithNoFunds() public {
+        vm.prank(creator);
+        vm.expectRevert("No funds to withdraw");
+        sugarDonation.withdrawCreatorFunds(address(token));
+    }
+}
